fe1ff906e94dd3ff00ccd22fbcb511ee621e1d1c
Md Kowsar Hossain <149518355+Kowsar-Hossain@users.noreply.github.com>
2025-05-14 18:51:15 +0600

    Include all signals

--- problem01.py
+++ problem01.py
@@ -3,22 +3,54 @@ import matplotlib.pyplot as plt
 
 n = np.arange(-10, 11, 1)
 
-plt.figure(figsize=(12,10))
+# unit step signal
+unit_step = np.where(n >= 0, 1, 0)
 
-unit_step = np.where(n >= 0 , 1 , 0)
-ramp = np.where(n >= 0, n, 0)                  # r[n] = n*u[n]
-exponential = np.where(n >= 0, 0.8**n, 0)      # a^n*u[n], here a=0.8
-sine = np.sin(0.2 * np.pi * n)                 # sin(0.2πn)
-cosine = np.cos(0.2 * np.pi * n)
+# ramp signal
+ramp = np.where(n >= 0, n, 0)
+
+# exponential signal
+alpha = -0.9
+exponential = alpha ** n
+
+# sine wave signal
+pi = np.pi
+A = 1
+f = .1
+fi = 0
+sine_signal = A * np.sin(2 * pi * f * n + fi)
+
+# cos wave signal
+cosine_signal = A * np.cos(2 * pi * f * n + fi)
+
+# Plot signals
+
+plt.figure(figsize=(12, 8))
 
 plt.subplot(3, 2, 1)
-plt.stem(n,unit_step)
-plt.title("Unit Step Sequence")
+plt.stem(n, unit_step)
+plt.title('Unit Step Signal')
 plt.grid(True)
-plt.show()
 
-plt.subplot(3, 2, 1)
-plt.stem(n,ramp)
-plt.title("Unit Step Sequence")
+plt.subplot(3, 2, 2)
+plt.stem(n, ramp)
+plt.title('Ramp Signal')
 plt.grid(True)
+
+plt.subplot(3, 2, 3)
+plt.stem(n, exponential)
+plt.title('Exponential Signal')
+plt.grid(True)
+
+plt.subplot(3, 2, 4)
+plt.stem(n, sine_signal)
+plt.title('Sine Wave Signal')
+plt.grid(True)
+
+plt.subplot(3, 2, 5)
+plt.stem(n, cosine_signal)
+plt.title('Cosine Wave Signal') 
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
