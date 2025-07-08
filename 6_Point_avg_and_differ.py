6d1ec1ae4c3a1f63979d836e39351e67ce36f1f5
Md Kowsar Hossain <149518355+Kowsar-Hossain@users.noreply.github.com>
2025-07-08 14:46:43 +0600

    6_Point_avg_and_differ

--- 6_Point_avg_and_differ.py
+++ 6_Point_avg_and_differ.py
@@ -1,47 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(0) #noise reproducible every time
+np.random.seed(0)
 n = np.linspace(0,1,200)
-x = np.sin(2*np.pi*5*n) + 0.5*np.random.rand(len(n))
+x = np.sin(2*np.pi*5*n) + 0.6*np.random.rand(len(n))
 
-#6-point averaging filter
-def avg_filter(x):
+def averaging(x):
     y = np.zeros_like(x)
     for i in range(5, len(x)):
-        y[i] = (x[i] + x[i-1] + x[i-2] + x[i-3] + x[i-4] + x[i-5])/6
-    return y
-
-#6-point differencing filter
-def differ_filter(x):
-    y = np.zeros_like(x)
-    for i in range(len(x)):
-        y[i] = (x[i] - x[i-1] + x[i-2] - x[i-3] + x[i-4] - x[i-5])/6
+        y[i] = x[i] + x[i-1] + x[i-2] + x[i-3] + x[i-4] + x[i-5]
     return y    
 
-#Plotting function
-def plot_signal(x,y,title,color='b'):
-    plt.plot(x,y,color = color)
-    plt.title(title)
-    plt.xlabel('Time(s)')
-    plt.ylabel('Amplitude')
-    plt.grid(True)
 
-#Apply filter
-y_avg = avg_filter(x)
-y_diff = differ_filter(x)
+def differencing(x):
+    y = np.zeros_like(x)
+    for i in range(5, len(x)):
+        y[i] = x[i] - x[i-1] + x[i-2] - x[i-3] + x[i-4] - x[i-5]
+    return y 
 
-#plot result
+avg_filter = averaging(x)
+diff_filter = differencing(x)
+
+#Plot Orginal Siganl
 plt.subplot(3, 1, 1)
-plot_signal(n, x, 'Orginal Analog Signal', 'b')
+plt.plot(n, x, 'g', label='Input signal')
+plt.title('Orginal Signal')
+plt.grid(True)
 
+#Plot Averaging Siganl
 plt.subplot(3, 1, 2)
-plot_signal(n, y_avg, '6-point Averaging Filter', 'y')
+plt.plot(n, avg_filter, 'r', label='Averaging signal')
+plt.title('Averaging Signal')
+plt.grid(True)
 
+#Plot Differencing Siganl
 plt.subplot(3, 1, 3)
-plot_signal(n, y_diff, '6-point Differencing Signal', 'r')
+plt.plot(n, diff_filter, 'b', label='Differencing signal')
+plt.title('Dufferencing Signal')
+plt.grid(True)
 
-
-plt.legend()
 plt.tight_layout()
+plt.legend()
 plt.show()
