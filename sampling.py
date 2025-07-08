1219bf753ce721b486ae94e8e4c887912be206f4
Md Kowsar Hossain <149518355+Kowsar-Hossain@users.noreply.github.com>
2025-07-08 10:49:18 +0600

    Sampling at 75 and 200 Hz

--- sampling.py
+++ sampling.py
@@ -1,36 +1,24 @@
-'''
-Consider the continuous-time analog signal x(t)=3cos(100πt). Sample the analog 
-signal at 200 Hz and 75 Hz. Show the discrete-time signal after sampling. ⟹ 
-realization. .
-
-'''
 import numpy as np
 import matplotlib.pyplot as plt
 
 n = np.linspace(0, 0.04, 1000)
+
 y = 3 * np.cos(100*np.pi*n)
-plt.plot(n, y, label = 'input signal')
-plt.xlabel('time')
-plt.ylabel('amplitude')
-plt.legend()
+plt.plot(n, y, label='input signal')
 
-
-
+#Sampling at 75Hz
 n = np.arange(0, 0.04, 1/75)
-y = y = 3 * np.cos(100*np.pi*n)
-plt.stem(n, y, 'g', label = 'sample 75',  basefmt=" ")
-plt.xlabel('time')
-plt.ylabel('amplitude')
-plt.legend()
+y = 3*np.cos(100*np.pi*n)
+plt.stem(n, y, 'g', label='Sample 75')
 
-
+#Sampling at 200Hz
 n = np.arange(0, 0.04, 1/200)
-y = y = 3 * np.cos(100*np.pi*n)
-plt.stem(n, y, 'b', label = 'sample 200')
+y = 3*np.cos(100*np.pi*n)
+plt.stem(n, y, 'b', label='Sample 200')
+
 plt.xlabel('time')
-plt.ylabel('amplitude')
+plt.ylabel('Amplitude')
 plt.legend()
 plt.grid(True)
-
 plt.tight_layout()
 plt.show()
